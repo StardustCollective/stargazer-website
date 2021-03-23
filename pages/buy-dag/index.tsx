0b5b03ddcb58a3d93fae234a58d1dd6a6ded54c7
assasinOOZ <assasinooz77@gmail.com>
2021-03-23 16:04:36 +0800

    feat: add buy dag page template

---
+++ pages/buy-dag/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Layout } from "@components/common";
+
+const BuyDag: React.FC = () => {
+  return <Layout>This is Buy Dag page</Layout>;
+};
+
+export default BuyDag;
