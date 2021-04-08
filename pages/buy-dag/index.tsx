25bcb6b665cb97bf36003de239bdc7c31e486dd5
assasinOOZ <assasinooz77@gmail.com>
2021-04-08 17:32:04 +0800

    feat: backend api integration

--- pages/buy-dag/index.tsx
+++ pages/buy-dag/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
+import { RootState } from "@redux/reducers";
 import { Layout } from "@components/common";
 import {
   BuyDagForm,
@@ -10,39 +12,63 @@ import {
 import styles from "./index.module.scss";
 
 const BuyDag: React.FC = () => {
-  const exampleJson = {
-    order: { token: "DAG", quantity: 1, amountUSD: 1 },
-    customer: {
-      email: "1",
-      firstName: "1",
-      lastName: "1",
-      address: { name: "1", line1: "1", city: "1", state: "1", zip: 1 },
-    },
-    paymethod: {
-      number: 1,
-      cvv: "123",
-      name: "1",
-      expYear: "1234",
-      expMonth: "12",
-      zip: 1,
-    },
+  const {
+    usdValue,
+    dagValue,
+    cardName,
+    cardNumber,
+    expiryDate,
+    cvv,
+    country,
+    address,
+    city,
+    postalCode,
+  } = useSelector((root: RootState) => root.buyDag);
+
+  const handleSubmitRequest = () => {
+    const body = {
+      order: { token: "DAG", quantity: dagValue, amountUSD: usdValue },
+      customer: {
+        email: "1",
+        firstName: "1",
+        lastName: "1",
+        address: {
+          name: "1",
+          line1: "1",
+          city: city,
+          state: country,
+          zip: postalCode,
+        },
+      },
+      paymethod: {
+        number: cardNumber,
+        cvv: cvv,
+        name: cardName,
+        expYear: `20${expiryDate.split("/")[1]}`,
+        expMonth: expiryDate.split("/")[0],
+        zip: postalCode,
+      },
+    };
+    fetch("https://www.stargazer.network/api/v1/buy-dag/purchase", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then(async (res) => {
+        console.log(await res.json());
+      })
+      .catch((err) => console.log(err));
   };
 
   const [step, setStep] = useState(1);
-  const [usdValue, setUsdValue] = useState(0);
-  const [dagValue, setDagValue] = useState(0);
-  const [name, setName] = useState("");
-  const [cardNumber, setCardNumber] = useState("");
-  const [expiryDate, setExpiryDate] = useState("");
-  const [cvv, setCvv] = useState("");
   return (
     <Layout>
       <div className={styles.pageWrapper}>
         {step === 1 && (
           <BuyDagForm
             nextStep={(usdValue, dagValue) => {
-              setUsdValue(usdValue);
-              setDagValue(dagValue);
               console.log(usdValue, dagValue);
               setStep(2);
             }}
@@ -51,10 +77,7 @@ const BuyDag: React.FC = () => {
         {step === 2 && (
           <BuyDagFormStep1
             nextStep={({ cardName, cardNumber, expiryDate, cvv }) => {
-              setName(cardName);
-              setCardNumber(cardNumber);
-              setExpiryDate(expiryDate);
-              setCvv(cvv);
+              console.log({ cardName, cardNumber, expiryDate, cvv });
               setStep(3);
             }}
           />
@@ -62,7 +85,10 @@ const BuyDag: React.FC = () => {
         {step === 3 && (
           <BuyDagFormStep2
             prevStep={() => setStep(2)}
-            nextStep={() => setStep(1)}
+            nextStep={({ country, address, city, postalCode }) => {
+              console.log({ country, address, city, postalCode });
+              handleSubmitRequest();
+            }}
           />
         )}
       </div>
